47e277a744f93a571862fe5a2ae9c18d26abd5d4
DESKTOP-0LK1UEL\pierl <pierluigi.lorrai@gmail.com>
2025-07-15 21:17:49 +0200

    script db da pulire per setting iniziale db

--- Script DB/04_batch_privileges.sql
+++
@@ -1,7 +0,0 @@
--- Eseguire come SYSTEM
-
-GRANT SELECT, UPDATE ON management_user.ORDERS TO batch_user;
-GRANT SELECT ON management_user.ORDER_ITEMS TO batch_user;
-GRANT SELECT, UPDATE ON management_user.PRODUCTS TO batch_user;
-
-COMMIT;
